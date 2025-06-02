c6bc7bcf5c70136d8b4670cb3dbe64151e024d94
bjoshua <joshua.budgen@ynov.com>
2025-06-02 09:38:14 +0200

    feat(datastruct.go): Models data file
    - For create user I add the table user in a struct for query's
    - I create all tables for after work

--- internal/models/datastruct.go
+++ internal/models/datastruct.go
@@ -9,3 +9,49 @@ type User struct {
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"createdAt"`
 }
+
+type Session struct {
+	ID           int       `json:"id"`
+	UserID       int       `json:"userID"`
+	SessionToken string    `json:"sessionToken"`
+	ExpiresAt    time.Time `json:"expiresAt"`
+}
+
+type Category struct {
+	ID  int    `json:"id"`
+	Nom string `json:"nom"`
+}
+
+type Post struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"userID"`
+	Titre     string    `json:"titre"`
+	Contenu   string    `json:"contenu"`
+	ImageURL  string    `json:"imageURL"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type PostCategory struct {
+	PostID      int `json:"postID"`
+	CategorieID int `json:"categorieID"`
+}
+
+type Comment struct {
+	ID        int       `json:"id"`
+	PostID    int       `json:"postID"`
+	UserID    int       `json:"userID"`
+	Contenu   string    `json:"contenu"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type LikePost struct {
+	UserID int    `json:"userID"`
+	PostID int    `json:"postID"`
+	Type   string `json:"type"`
+}
+
+type LikeComment struct {
+	UserID    int    `json:"userID"`
+	CommentID int    `json:"commentID"`
+	Type      string `json:"type"`
+}

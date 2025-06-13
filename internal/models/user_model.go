c283a0bae30f2ae5b98cea3e19bca64674f8212b
Loic ANDRIANARIVONY <loic.andrianarivony@ynov.com>
2025-06-13 12:54:30 +0200

    feat(user_models.go): add GitHub user fields and GitHubUserInfo struct

--- internal/models/user_model.go
+++ internal/models/user_model.go
@@ -6,6 +6,20 @@ type User struct {
 	UserID    int       `json:"userid"`
 	Username  string    `json:"username"`
 	Email     string    `json:"email"`
-	Password  string    `json:"password"`
+	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"createdat"`
+
+	GitHubID     string    `json:"github_id,omitempty"`
+	Avatar       string    `json:"avatar,omitempty"`
+	IsVerified   bool      `json:"is_verified"`
+	AuthProvider string    `json:"auth_provider"`
+	UpdatedAt    time.Time `json:"updated_at,omitempty"`
+}
+
+type GitHubUserInfo struct {
+	ID     int    `json:"id"`
+	Login  string `json:"login"`
+	Email  string `json:"email"`
+	Name   string `json:"name"`
+	Avatar string `json:"avatar_url"`
 }
